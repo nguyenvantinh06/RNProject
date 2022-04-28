b30d2edbb3b296b9ae5ea0a2bd49e00083a8d76d
Nguyễn Văn Tĩnh <nguyenvantinh06@gmail.com>
2022-04-28 17:38:28 +0700

    Update initPushNotification

--- src/utils/FCMService.js
+++ src/utils/FCMService.js
@@ -1,8 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
+import PushNotificationIOS from "@react-native-community/push-notification-ios";
+import PushNotification from "react-native-push-notification";
 
 export async function requestUserPermission() {
-  const authStatus = await messaging().requestPermission();
+  const authStatus = await messaging().requestPermission({
+    alert: true,
+    badge: true,
+    sound: true,
+  });
   const enabled =
     authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
     authStatus === messaging.AuthorizationStatus.PROVISIONAL;
@@ -57,4 +63,117 @@ export const NotificationListener = () => {
     console.log('notification on foreground state', remoteMessage);
   });
 };
+
+export const initPushNotification = (onNotification = () => { }) => {
+  PushNotification.configure({
+
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: (token) => {
+      console.log("TOKEN:", token);
+    },
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: (notification) => {
+      console.log("NOTIFICATION:", notification);
+
+      //process the notification
+      onNotification(notification);
+      // (required) Called when a remote is received or opened, or local notification is opened
+      notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
+    onAction: (notification) => {
+      console.log("ACTION:", notification.action);
+      console.log("NOTIFICATION:", notification);
+
+      // process the action
+    },
+
+    // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
+    onRegistrationError: (err) => {
+      console.error(err.message, err);
+    },
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    // permissions: {
+    //   alert: true,
+    //   badge: true,
+    //   sound: true,
+    // },
+
+    // Should the initial notification be popped automatically
+    // default: true
+    popInitialNotification: true,
+
+    /**
+     * (optional) default: true
+     * - Specified if permissions (ios) and token (android and ios) will requested or not,
+     * - if not, you must call PushNotificationsHandler.requestPermissions() later
+     * - if you are not using remote notification or do not have Firebase installed, use this:
+     *     requestPermissions: Platform.OS === 'ios'
+     */
+    requestPermissions: true,
+  });
+
+  // Register background handler
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+    onNotification(remoteMessage, 'background');
+  });
+
+  PushNotification.createChannel(
+    {
+      channelId: "default-channel-id", // (required)
+      channelName: "Default channel", // (required)
+      channelDescription: "A default channel", // (optional) default: undefined.
+      soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+      playSound: true,
+      // importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+      // vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+    },
+    (created) => console.log(`createChannel 'default-channel-id' returned '${created}'`), // (optional) callback returns whether the channel was created, false means it already existed.
+  );
+
+};
+
+/**
+   * Check if your app has been granted permission to display notifications.
+   */
+export const checkAndAskPermission = (callback = () => { }) => {
+  messaging()
+    .hasPermission()
+    .then(res => {
+      console.log('permission fcm', res);
+      if (res) {
+        callback(true);
+      } else {
+        callback(false);
+      }
+    })
+    .catch(err => {
+      console.log('permission fcm', err);
+      callback(false);
+    });
+};
+
+export const onMessage = (callback = () => { }) => {
+  messaging().onMessage(remoteMessage => {
+    console.log('fcm test', remoteMessage);
+    callback(remoteMessage);
+  });
+};
+
+export const setBadgeIos = (number) => {
+  try {
+    PushNotificationIOS.setApplicationIconBadgeNumber(Number(number));
+  }
+  catch (error) { }
+};
+
+export const subscribeTopic = topic => {
+  return messaging().subscribeToTopic(topic);
+}
+
+
 
